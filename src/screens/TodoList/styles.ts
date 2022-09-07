98d3b0c58dfe2d3537a8d12fc12f727c5b174bdb
Saulo Magalhães <saulo.magalhaes@outlook.com.br>
2022-09-07 11:23:44 -0300

    feat(styles): :lipstick: implementando estilos naseção da logo

---
+++ src/screens/TodoList/styles.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  boxBlack: {
+    backgroundColor: '#0D0D0D',
+    width: "100%",
+    height: 173,
+    padding: 24,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  logo: {
+    width: 110,
+    height: 32,
+  },
+});
