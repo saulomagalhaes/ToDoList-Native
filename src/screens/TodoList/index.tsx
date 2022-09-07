a9e616bd1ff8b244e644c5bf3f3b81ea13bf4d97
Saulo Magalhães <saulo.magalhaes@outlook.com.br>
2022-09-07 20:17:26 -0300

    feat(TodoList): :lipstick: implementando estilos para a sessão de atividades criadas e concluídas

--- src/screens/TodoList/index.tsx
+++ src/screens/TodoList/index.tsx
@@ -1,11 +1,52 @@
-import { Image, View } from "react-native";
+import { Image, Text, TextInput, TouchableOpacity, View } from "react-native";
 
 import { styles } from "./styles";
 
 export function TodoList() {
   return (
-    <View style={styles.boxBlack}>
-      <Image style={styles.logo} source={require("../../../assets/Logo.png")} />
-    </View>
+    <>
+      <View style={styles.boxBlack}>
+        <Image
+          style={styles.logo}
+          source={require("../../../assets/Logo.png")}
+        />
+      </View>
+
+      <View style={styles.container}>
+        <View style={styles.addTask}>
+          <TextInput
+            style={styles.input}
+            placeholder="Adicione uma nova tarefa"
+            placeholderTextColor="#808080"
+          />
+
+          <TouchableOpacity style={styles.button}>
+            <Image source={require("../../../assets/plus.png")} />
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.status}>
+
+          <View style={styles.box}>
+            <View style={styles.boxText}>
+              <Text style={styles.textCreated}>Criadas</Text>
+            </View>
+            <View style={styles.boxSpan}>
+              <Text style={styles.span}>0</Text>
+            </View>
+          </View>
+
+          <View style={styles.box}>
+            <View style={styles.boxText}>
+              <Text style={styles.textFinished}>Criadas</Text>
+            </View>
+            <View style={styles.boxSpan}>
+              <Text style={styles.span}>0</Text>
+            </View>
+          </View>
+
+        </View>
+      </View>
+    </>
   );
 }
--- src/screens/TodoList/styles.ts
+++ src/screens/TodoList/styles.ts
@@ -13,4 +13,57 @@ export const styles = StyleSheet.create({
     width: 110,
     height: 32,
   },
+  container: {
+    padding: 24,
+    backgroundColor: '#1A1A1A',
+    height: '100%',
+  },
+  addTask: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    width: '100%',
+    marginTop: -50,
+  },
+  input: {
+    backgroundColor: '#262626',
+    color:'#454545',
+    paddingStart: 16,
+    paddingEnd: 63,
+    marginEnd: 4,
+    borderRadius: 6,
+    flex: 1,
+  },
+  button: {
+    backgroundColor: '#1E6F9F',
+    padding: 18,
+    borderRadius: 6,
+  },
+  status: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginTop: 32,
+    width: '100%',
+  },
+  box: {
+    flexDirection: 'row',
+  },
+  boxText: {
+    marginRight: 8,
+  },
+  boxSpan: {
+    backgroundColor: '#333333',
+    width:25,
+    borderRadius: 9,
+  },
+  textCreated: {
+    color: '#4EA8DE',
+  },
+  textFinished: {
+    color: '#8284FA',
+  },
+  span: {
+    color: '#FFFFFF',
+    textAlign: 'center',
+  },
 });
